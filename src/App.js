c15a2e2178ccfa52615d1abba3adac4d91885548
Kaveh <rezarezaeijamiuk@gmail.com>
2023-08-20 09:40:12 +0100

    home and cart routes created through rrd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-    
+      <Router>
+        <Routes>
+          <Route path="/" />
+          <Route path="/cart" />
+        </Routes>
+      </Router>
     </div>
   );
 }
