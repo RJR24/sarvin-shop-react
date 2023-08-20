f19bd3b50aa9bf389ca44fea41af757f4672fa34
Kaveh <rezarezaeijamiuk@gmail.com>
2023-08-20 12:50:13 +0100

    product data added

---
+++ src/data/products.js
@@ -0,0 +1,37 @@
+import mobile from "assets/images/mobile";
+import laptop from "assets/images/laptop";
+import shoes from "assets/images/shoes";
+import camera from "assets/images/camera";
+import jeans from "assets/images/jeans";
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Mobile",
+    price: 1000,
+    productImage: mobile,
+  },
+  {
+    id: 2,
+    productName: "Jeans",
+    price: 400,
+    productImage: jeans,
+  },
+  {
+    id: 3,
+    productName: "Camera",
+    price: 1500,
+    productImage: camera,
+  },
+  {
+    id: 4,
+    productName: "Laptop",
+    price: 2000,
+    productImage: laptop,
+  },
+  {
+    id: 5,
+    productName: "Shoes",
+    price: 500,
+    productImage: shoes,
+  },
+];
