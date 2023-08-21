572e79977159343bcbbafacd039c2586de08fe35
Kaveh <rezarezaeijamiuk@gmail.com>
2023-08-21 23:23:31 +0100

    working on cart component

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,4 +1,20 @@
+import React, { useContext } from "react";
+import { PRODUCTS } from "../../data/products";
+import { ShopContext } from "../../context/shopContext";
+import Products from "../shop/Products";
+
 const Cart = () => {
-  return <h1>Cart</h1>;
+  const { cartItems } = useContext(ShopContext);
+  return (
+    <React.Fragment>
+      <h1>Selected Items In Your Cart</h1>
+      <div className="row">
+        {PRODUCTS.map((p) => {
+          if (cartItems.some((i) => i.id === p.id && i.count > 0))
+            return <Products data={p}></Products>;
+        })}
+      </div>
+    </React.Fragment>
+  );
 };
 export default Cart;
