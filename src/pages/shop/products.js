a9522f9eba22c272e81684885ba4f11f9426e9a6
Kaveh <rezarezaeijamiuk@gmail.com>
2023-08-21 22:37:39 +0100

    using shop context to handel the products

--- src/pages/shop/products.js
+++ src/pages/shop/products.js
@@ -1,14 +1,27 @@
+import { useContext } from "react";
+import { ShopContext } from "../../context/shopContext";
+
 const Products = (props) => {
   const { id, productName, productImage, price } = props.data;
+  const { removeFromCart, addToCart, cartItems } = useContext(ShopContext);
   return (
     <div className="col-3">
       <img src={productImage} alt="product" className="w-50" />
 
       <h5>{productName}</h5>
       <p>price: {price}£</p>
-      <button className="btn btn-info btn-sm">-</button>
-      <span className="mx-2">0</span>
-      <button className="btn btn-info btn-sm">+</button>
+      <button
+        className="btn btn-info btn-sm"
+        onClick={() => removeFromCart(id)}
+      >
+        -
+      </button>
+      <span className="mx-2">
+        {cartItems?.filter((row) => row.id === id)[0]?.count}
+      </span>
+      <button className="btn btn-info btn-sm" onClick={() => addToCart(id)}>
+        +
+      </button>
     </div>
   );
 };
