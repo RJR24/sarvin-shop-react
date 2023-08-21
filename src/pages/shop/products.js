672e3720417f719947df5022e9b2d74aa499596f
Kaveh <rezarezaeijamiuk@gmail.com>
2023-08-21 22:52:59 +0100

    removeFromCart button function improved

--- src/pages/shop/products.js
+++ src/pages/shop/products.js
@@ -4,18 +4,21 @@ import { ShopContext } from "../../context/shopContext";
 const Products = (props) => {
   const { id, productName, productImage, price } = props.data;
   const { removeFromCart, addToCart, cartItems } = useContext(ShopContext);
+  const isInCart = cartItems?.some((item) => item.id === id);
   return (
     <div className="col-3">
       <img src={productImage} alt="product" className="w-50" />
 
       <h5>{productName}</h5>
       <p>price: {price}£</p>
-      <button
-        className="btn btn-info btn-sm"
-        onClick={() => removeFromCart(id)}
-      >
-        -
-      </button>
+      {isInCart && (
+        <button
+          className="btn btn-info btn-sm"
+          onClick={() => removeFromCart(id)}
+        >
+          -
+        </button>
+      )}
       <span className="mx-1">
         {cartItems?.filter((row) => row.id === id)[0]?.count}
       </span>
