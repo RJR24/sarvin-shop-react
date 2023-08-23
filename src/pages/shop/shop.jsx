34eb78b2934af916fe66777fb4307f4de50c4da9
Kaveh <rezarezaeijamiuk@gmail.com>
2023-08-23 16:28:06 +0100

    products.js name changed

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import Products from "./Products";
+import Products from "./products";
 import { PRODUCTS } from "../../data/products";
 const Shop = () => {
   return (
