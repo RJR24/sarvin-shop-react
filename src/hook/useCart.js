5f69015e79da8dc6ae1c3133a1f28d5b10f89d21
Kaveh <rezarezaeijamiuk@gmail.com>
2023-08-23 16:27:02 +0100

    useCart.js

---
+++ src/hook/useCart.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export const useCart = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (itemId) => {
+    if (!cartItems?.find((item) => item.id === itemId))
+      setCartItems([...cartItems, { id: itemId, count: 1 }]);
+    else
+      setCartItems(
+        cartItems.map((item) => {
+          if (item.id === itemId) return { ...item, count: item.count + 1 };
+          else return item;
+        })
+      );
+    console.log(cartItems);
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems(
+      cartItems.map((item) => {
+        if (item.id === itemId)
+          return { ...item, count: item.count === 0 ? 0 : item.count - 1 };
+        else return item;
+      })
+    );
+  };
+  return { cartItems, removeFromCart, addToCart };
+};
