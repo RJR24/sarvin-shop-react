f2d69c3168bb76712b73382703b90eef547ebae3
Kaveh <rezarezaeijamiuk@gmail.com>
2023-10-11 12:37:20 +0100

    cat list updated styles added

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,20 +1,31 @@
 import React from "react";
-import mobile from "../../assets/images/products/mobile.png";
+
+import smartPhone from "../../assets/images/pro02/smart-phone.png";
+import smartWatch from "../../assets/images/pro02/smart-watch.png";
+import camera from "../../assets/images/pro02/camera.png";
+import accessories from "../../assets/images/pro02/accessories.png";
+import gaming from "../../assets/images/pro02/gaming.png";
+import laptop from "../../assets/images/pro02/laptop.png";
 import CategoryCard from "./CategoryCard";
 
+import "./category.css";
+
 const catList = [
-  { title: "Mobile", image: mobile },
-  { title: "Laptop", image: mobile },
-  { title: "Mobile", image: mobile },
-  { title: "Mobile", image: mobile },
-  { title: "Mobile", image: mobile },
+  { title: "Accessories", image: accessories },
+  { title: "Camera", image: camera },
+  { title: "Laptop", image: laptop },
+  { title: "Smart Phone", image: smartPhone },
+  { title: "Gaming", image: gaming },
+  { title: "Smart Watch", image: smartWatch },
 ];
 const Categories = () => {
-  return <div  className="d-flex">
-    {
-      catList.map((item)=><CategoryCard  title={item.title} image={item.image}/>)
-    }
-  </div>;
+  return (
+    <div className="d-flex justify-content-center gap-3">
+      {catList.map((item) => (
+        <CategoryCard title={item.title} image={item.image} />
+      ))}
+    </div>
+  );
 };
 
 export default Categories;
