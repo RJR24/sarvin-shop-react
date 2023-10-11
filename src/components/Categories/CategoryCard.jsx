95c3879c7d544ed719b4f0e1e6f3d5606dc67c50
Kaveh <rezarezaeijamiuk@gmail.com>
2023-10-11 12:37:47 +0100

    working on styles

--- src/components/Categories/CategoryCard.jsx
+++ src/components/Categories/CategoryCard.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./category.css";
 
 const CategoryCard = ({ title, image }) => {
   return (
-    <div>
-      <img src={image} alt="" width="200" />
+    <div className="cat">
+      <img src={image} alt="" width="80px" /> <br />
       <span>{title}</span>
     </div>
   );
