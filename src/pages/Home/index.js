89e1339035cdbad1f97272b329322a785321a207
Kaveh <rezarezaeijamiuk@gmail.com>
2023-10-16 11:47:32 +0100

    home page updated by importing components

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,30 @@
 import React from "react";
 import Categories from "../../components/Categories/Categories";
 import OnSaleProducts from "../../components/OnSale/DiscountedProducts";
+import NewProducts from "../../components/NewProducts/NewProducts";
+import BestSellersList from "../../components/Best-seller-products/NewBestSeller.jsx";
+
+import banner from "../../assets/Frame 26086920.png"
+import TopBrands from "../../components/TopBrands/TopBrands";
+import SmartWatch from "../../components/smartWatchBanner/SmartWatchBanner.jsx";
+import NewBlogs from "../../components/OurBlogSecttion/OurBlog";
 
 const Home = () => {
   return (
-    <div>
+    <div className="container">
       <Categories />
       <OnSaleProducts />
+      <NewProducts />
+      <div className="container d-flex justify-content-center"><img src={banner} alt="banner"/></div>
+      <BestSellersList />
+      <TopBrands />
+      <SmartWatch />
+      <NewBlogs />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 };
