320705440725a34fb03ab0f711333301a4bcc6bb
Kaveh <rezarezaeijamiuk@gmail.com>
2023-10-20 10:10:56 +0100

    working on hero section

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import "./heroSectionStyles.css";
 
 import HeroImage from "../../assets/Hero-section.png";
 
 const Hero = () => {
   return (
-    <div className="hero-section">
-      <div>
-        <div className="shop-name">Tech Heim</div>
-        <div className="techHeim-slogan">"Join the digital revolution"</div>
-
-        <button className="explore-button">Explore more</button>
+    <div className="hero-container d-flex justify-content-between flex-start">
+      <div className=" d-flex flex-column justify-content-between gap-5">
+        <div className="d-flex flex-column gap-5">
+          <div className="company-name">Tech Heim</div>
+          <div className="company-slogan ">
+            <span style={{"color": "#042352"}}>"Join the</span> <span style={{"color": "#F45E0C"}}>digital revolution"</span>
+          </div>
+        </div>
+        <div className=" ">
+          <button className="explore-button d-flex">Explore more</button>
+        </div>
       </div>
       <div>
         <img src={HeroImage} alt="laptop" />
