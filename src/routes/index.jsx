16022e65192ec3b2f68e47e1cabd9477b21d0166
Kaveh <rezarezaeijamiuk@gmail.com>
2023-10-20 10:12:09 +0100

    /product added to the routes of the project

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Shop from "../pages/shop/shop";
 import Cart from "../pages/cart/cart";
 import Home from "../pages/Home";
+import Product from "../pages/Product1/Product-laptop";
 
 const RoutesMap = () => {
   return (
@@ -10,6 +11,7 @@ const RoutesMap = () => {
       <Route path="/" element={<Home />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/shop" element={<Shop />} />
+      <Route path="/product" element={<Product />} />
     </Routes>
   );
 };
