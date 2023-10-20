9e15dedf9b2bb7fb842b282aed9372b0f7512c3f
Kaveh <rezarezaeijamiuk@gmail.com>
2023-10-20 10:13:21 +0100

    product page created

---
+++ src/pages/Product1/Product-laptop.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Product from "../../components/Product/Product";
+
+const Product1 = () => {
+  return (
+    <div className="container">
+      <Product />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+  );
+};
+
+export default Product1;
