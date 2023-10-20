a1a78a293b71f1861b26ca2c66404f22342760a8
Kaveh <rezarezaeijamiuk@gmail.com>
2023-10-20 16:23:14 +0100

    provider set store/footer imported and used

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 import "./assets/css/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router} from "react-router-dom";
+
+import { Provider } from "react-redux";
+
+import { BrowserRouter as Router } from "react-router-dom";
 import Nav from "./components/nav";
 import { ShopContextProvider } from "./context/shopContext";
 import Welcome from "./components/welcome";
 import RoutesMap from "./routes";
 import Header from "./components/layouts/Header";
-
-
+import Footer from "./components/layouts/FooterSection/Footer";
+import store from "./redux/Store";
 
 function App() {
   return (
     <div className="App">
+      <Provider store={store}>
       <ShopContextProvider>
         <Router>
           <Header />
           <Welcome />
-         <RoutesMap/>
+          <RoutesMap />
+          <Footer />
         </Router>
       </ShopContextProvider>
+      </Provider>
     </div>
   );
 }
 
 export default App;
-
 
 // sass , tailwind, typescript, redux
