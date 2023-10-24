37a750927c6c9b37f746c26b273a8f48d1e2af6b
Kaveh <rezarezaeijamiuk@gmail.com>
2023-10-24 17:50:38 +0100

    working on styles and responsiveness

--- src/components/Services/OurServices.jsx
+++ src/components/Services/OurServices.jsx
@@ -9,40 +9,77 @@ import guarantee from "../../assets/images/icons/guarantee.svg";
 
 const OurServices = () => {
   return (
-    <div className="servicesContainer">
-      <div className="services">
+    <>
+      <div className="row servicesContainer d-none d-md-flex">
+        <div className="col-sm-12 col-md-3 services">
+          <div className="image">
+            <img src={techNewsIcon} alt="screen" />
+          </div>
+          <div className="text">
+            <span>Latest and Greatest Tech</span>
+          </div>
+        </div>
+        <div className="col-sm-12 col-md-3 services">
+          <div className="image">
+            <img src={guarantee} alt="guarantee" />
+          </div>
+          <div className="text">
+            <span>Guarantee</span>
+          </div>
+        </div>
+        <div className="col-sm-12 col-md-3 services">
+          <div className="image">
+            <img src={shipping} alt="shipping" />
+          </div>
+          <div className="text">
+            <span>Free Shipping over 1000$</span>
+          </div>
+        </div>
+        <div className="col-sm-12 col-md-3 services">
+          <div className="image">
+            <img src={clock} alt="clock" />
+          </div>
+          <div className="text">
+            <span>24/7 Support</span>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="row servicesContainer2">
+      <div className="col-sm-12 services">
        <div className="image">
          <img src={techNewsIcon} alt="screen" />
         </div>
-       <div className="p">
-         <p>Latest and Greatest Tech</p>
+       <div className="text">
+         <span>Latest and Greatest Tech</span>
         </div>
       </div>
-      <div className="services">
+      <div className="col-sm-12 services">
        <div className="image">
          <img src={shipping} alt="shipping" />
         </div>
-       <div className="p">
-         <p>Guarantee</p>
+       <div className="text">
+         <span>Guarantee</span>
         </div>
       </div>
-      <div className="services">
+      <div className="col-sm-12 services">
        <div className="image">
          <img src={guarantee} alt="guarantee" />
         </div>
-       <div className="p">
-         <p>Free Shipping over 1000$</p>
+       <div className="text">
+         <span>Free Shipping over 1000$</span>
         </div>
       </div>
-      <div className="services">
+      <div className="col-sm-12 services">
        <div className="image">
          <img src={clock} alt="clock" />
         </div>
-       <div className="p">
-         <p>24/7 Support</p>
+       <div className="text">
+         <span>24/7 Support</span>
         </div>
       </div>
-    </div>
+    </div> */}
+    </>
   );
 };
 
