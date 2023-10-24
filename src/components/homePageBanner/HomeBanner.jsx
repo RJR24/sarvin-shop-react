670d1fcf400d14b9b478804480ba582c0ef0bf06
Kaveh <rezarezaeijamiuk@gmail.com>
2023-10-24 10:12:02 +0100

    working on improving grid style & responsiveness

--- src/components/homePageBanner/HomeBanner.jsx
+++ src/components/homePageBanner/HomeBanner.jsx
@@ -9,23 +9,22 @@ import iphone15Mobile from "../../assets/images/banners/iphone15-mobile.svg";
 
 const HomeBanner = () => {
   return (
-    <div className="row d-flex justify-content-center gap-2">
+    <div className="row d-flex justify-content-center ">
       <div className="iphone15 col-md-7 col-12 ">
         <picture>
-          {/* <source
-            srcSet="../../assets/images/banners/iphone15.png"
-            media="(min-width : 768px)"
-          /> */}
+          <source srcSet={iphone15} media="(min-width : 768px)" />
           <img src={iphone15Mobile} alt="iphone15" width="100%" />
         </picture>
       </div>
       <div className="ps5  col-md-4 col-12">
         <picture>
-          {/* <source
-            srcSet="../../assets/images/banners/ps501.png"
-            media="(min-width:768px)"
-          /> */}
-          <img src={ps5Mobile} alt="play station" width="100%" />
+          <source srcSet={ps5} media="(min-width:768px )" />
+          <img
+            src={ps5Mobile}
+            alt="play station"
+            width="100%"
+            height="-webkit-fill-available"
+          />
         </picture>
       </div>
     </div>
