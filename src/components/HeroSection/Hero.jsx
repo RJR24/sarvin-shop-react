dda2c462a8fb0be5fd89aa4f2a36b862eefd9bd6
Kaveh <rezarezaeijamiuk@gmail.com>
2023-10-25 17:23:03 +0100

    working on style and responsiveness

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -6,8 +6,8 @@ import HeroImage from "../../assets/Hero-section.png";
 const Hero = () => {
   return (
     <div className=" row hero-container d-flex justify-content-between">
-      <div className="w-auto col-sm-4 d-flex flex-column justify-content-between gap-5">
-        <div className="d-flex flex-column gap-5">
+      <div className="col col-md-5 d-flex flex-column justify-content-between gap-3 gap-md-5">
+        <div className="d-flex flex-column  gap-3 gap-md-5">
           <div className="company-name">Tech Heim</div>
           <div className="company-slogan ">
             <div style={{ color: "#042352" }}>
@@ -20,8 +20,8 @@ const Hero = () => {
           <button className="explore-button d-flex">Explore more</button>
         </div>
       </div>
-      <div className="heroImage col-sm-8">
-        <img src={HeroImage} alt="laptop"  />
+      <div className="col col-md-7 heroImage">
+        <img src={HeroImage} alt="laptop"  className="w-100" />
       </div>
     </div>
   );
