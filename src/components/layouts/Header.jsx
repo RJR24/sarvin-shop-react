14a992d59a09c2ceea92234353f24ef235698028
Kaveh <rezarezaeijamiuk@gmail.com>
2023-10-25 17:26:18 +0100

    working on logout function

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -10,13 +10,19 @@ import profile from "../../assets/images/icons/profile-circle.svg";
 import orders from "../../assets/images/icons/orders.svg";
 import wishList from "../../assets/images/icons/wishList.svg";
 import payments from "../../assets/images/icons/payments.svg";
-import logout from "../../assets/images/icons/logout.svg";
+import logoutIcon from "../../assets/images/icons/logout.svg";
 import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout } from "../../redux/slices/userSlice";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
   const username = useSelector((state) => state.userReducer.username);
+  const dispatcher = useDispatch();
+  const userLogin = () => {
+    dispatcher(login({ username: "Kaveh024" }));
+  };
+
   return (
     <header className=" header-styles py-3 ">
       <div className="container d-flex justify-content-between">
@@ -80,12 +86,14 @@ const Header = () => {
                     <DropdownItem img={orders} text={"Orders"} />
                     <DropdownItem img={wishList} text={"Wish List"} />
                     <DropdownItem img={payments} text={"Payments"} />
-                    <DropdownItem img={logout} text={"Log Out"} />
+                    <DropdownItem img={logoutIcon} text={"Log Out"}  event={true} />
                   </ul>
                 </div>
               ) : (
                 <div>
-                  <button className="btn btn-primary">Login</button>
+                  <button className="btn btn-primary" onClick={userLogin}>
+                    Login
+                  </button>
                   <button className="btn btn-primary">Register</button>
                 </div>
               )}
@@ -98,11 +106,24 @@ const Header = () => {
 };
 
 function DropdownItem(props) {
+  const dispatcher = useDispatch();
+  const userLogout = () => {
+    dispatcher(logout());
+  };
   return (
-    <li className="dropdownItem">
-      <img src={props.img} alt="" />
-      <a href="#">{props.text}</a>
-    </li>
+    <>
+      {props.event ? (
+        <li className="dropdownItem" onClick={userLogout}>
+          <img src={props.img} alt="" />
+          <a href="#">{props.text}</a>
+        </li>
+      ) : (
+        <li className="dropdownItem">
+          <img src={props.img} alt="" />
+          <a href="#">{props.text}</a>
+        </li>
+      )}
+    </>
   );
 }
 
