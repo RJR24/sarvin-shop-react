ecad2d07659674b43ba4d564e8d2b975bc1b6bc7
Kaveh <rezarezaeijamiuk@gmail.com>
2023-10-27 14:55:00 +0100

    on Sale products file name changed

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Categories from "../../components/Categories/Categories";
-import OnSaleProducts from "../../components/OnSale/DiscountedProducts";
+import OnSaleProducts from "../../components/OnSaleProducts/DiscountedProducts";
 import NewProducts from "../../components/NewProducts/NewProducts";
 import BestSellersList from "../../components/Best-seller-products/NewBestSeller.jsx";
 
@@ -10,7 +10,7 @@ import SmartWatch from "../../components/smartWatchBanner/SmartWatchBanner.jsx";
 import NewBlogs from "../../components/OurBlogSection/OurBlog";
 import OurServices from "../../components/Services/OurServices";
 import Hero from "../../components/HeroSection/Hero";
-import HomeBanner from "../../components/homePageBanner/HomeBanner";
+import HomeBanner from "../../components/homePageBanner/HomeBanner"
 
 
 const Home = () => {
