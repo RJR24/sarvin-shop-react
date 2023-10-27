f8f953380959b7c7297b665a3ff64ce7069cd41b
Kaveh <rezarezaeijamiuk@gmail.com>
2023-10-27 12:14:31 +0100

    working on hero section responsiveness

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -6,12 +6,12 @@ import HeroImage from "../../assets/Hero-section.png";
 const Hero = () => {
   return (
     <div className=" row hero-container d-flex justify-content-between">
-      <div className="col col-md-5 d-flex flex-column justify-content-between gap-3 gap-md-5">
+      <div className="col-5  d-flex flex-column justify-content-between gap-3 gap-md-5">
         <div className="d-flex flex-column  gap-3 gap-md-5">
           <div className="company-name">Tech Heim</div>
           <div className="company-slogan ">
             <div style={{ color: "#042352" }}>
-              "Join the 
+              "Join the
               <span style={{ color: "#F45E0C" }}> digital revolution"</span>
             </div>{" "}
           </div>
@@ -21,7 +21,7 @@ const Hero = () => {
         </div>
       </div>
       <div className="col col-md-7 heroImage">
-        <img src={HeroImage} alt="laptop"  className="w-100" />
+        <img src={HeroImage} alt="laptop" className="w-100" />
       </div>
     </div>
   );
