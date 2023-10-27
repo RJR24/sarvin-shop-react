45508dde37664fc2092caf1e902f0f71c952bf54
Kaveh <rezarezaeijamiuk@gmail.com>
2023-10-27 14:53:50 +0100

    readability improved

--- src/components/Categories/CategoryCard.jsx
+++ src/components/Categories/CategoryCard.jsx
@@ -3,7 +3,7 @@ import "./category.css";
 
 const CategoryCard = ({ title, image }) => {
   return (
-    <div className="cat col-sm-5 col-md-1 ">
+    <div className="cat d-flex flex-column align-items-center col-sm-3 col-md-1 ">
       <img src={image} alt="" width="100px" /> <br />
       <span>{title}</span>
     </div>
--- src/hook/useCart.js
+++ src/hook/useCart.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export const useCart = () => {
-  const [cartItems, setCartItems] = useState(); 
+  const [cartItems, setCartItems] = useState();
 
   useEffect(() => {
     const data = localStorage.getItem("Sarvin_shop_cart_items");
@@ -31,7 +31,7 @@ export const useCart = () => {
         if (item.id === itemId)
           return { ...item, count: item.count === 0 ? 0 : item.count - 1 };
         else return item;
-      }) 
+      })
     );
   };
   const clearShoppingCart = () => {
