e0aadb49dbd10f157af229651e3aba7316650514
Kaveh <rezarezaeijamiuk@gmail.com>
2023-10-27 12:17:46 +0100

    working on implementing carousel to the component

--- src/components/OnSaleProducts/DiscountedProducts.jsx
+++ src/components/OnSaleProducts/DiscountedProducts.jsx
@@ -1,4 +1,6 @@
-import React, { Fragment } from "react";
+import "react-multi-carousel/lib/styles.css";
+import Carousel from "react-multi-carousel";
+import "./onSaleProductsStyles.css";
 import ProductOnSaleCard from "./ProductOnSaleCard";
 
 import mouse from "../../assets/images/pro02/mouse.png";
@@ -6,8 +8,6 @@ import keyboard from "../../assets/images/pro02/keyboard.png";
 import appleWatch from "../../assets/images/pro02/appleWatch.png";
 import macBook from "../../assets/images/pro02/macBook.png";
 import arrowCircleRight from "../../assets/images/arrows-btn-etc/arrow-circle-right.svg";
-
-import "./onSaleProductsStyles.css";
 
 const onSaleProductsList = [
   {
@@ -28,26 +28,54 @@ const onSaleProductsList = [
     data: { price: 100, specification: "model ...", discount: 0 },
     image: mouse,
   },
+  {
+    data: { price: 100, specification: "model ...", discount: 0 },
+    image: macBook,
+  },
+  {
+    data: { price: 100, specification: "model ...", discount: 0 },
+    image: mouse,
+  },
+  {
+    data: { price: 100, specification: "model ...", discount: 0 },
+    image: macBook,
+  },
 ];
 const OnSaleProducts = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 1024 },
+      items: 4,
+    },
+    desktop: {
+      breakpoint: { max: 1024, min: 800 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 800, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
   return (
-    // <Fragment>
+    // <>
     //   <div>
     //     <p>Products On Sale</p>
     //     <p>Shop Now!</p>
     //     <button className="btn ">
     //       View all <img src={arrowCircleRight} alt="right arrow" />
     //     </button>
-      // </div>
-      <div className="pOnSaleContainer">
-        {onSaleProductsList.map((item) => (
-          <ProductOnSaleCard
-            data={item.data.specification}
-            image={item.image}
-          />
-        ))}
-      </div>
-    // </Fragment>
+    // </div>
+    <Carousel className="pOnSaleContainer" responsive={responsive}>
+      {onSaleProductsList.map((item) => (
+        <ProductOnSaleCard data={item.data} image={item.image} />
+      ))}
+    </Carousel>
+    // </>
   );
 };
 
