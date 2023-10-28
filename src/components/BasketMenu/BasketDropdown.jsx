c21c4549e8bfe521fd0162e5f4c94156e9e8249b
Kaveh <rezarezaeijamiuk@gmail.com>
2023-10-28 11:50:36 +0100

    basket drop down menu component started

---
+++ src/components/BasketMenu/BasketDropdown.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import basketItemsCard from "./basketItemsCard";
+
+const BasketDropdown = () => {
+  return (
+  <div>
+    
+  </div>)
+};
+
+export default BasketDropdown;
