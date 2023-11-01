3d88359d97a28bf79788f8dc5c559465963012d0
Kaveh <rezarezaeijamiuk@gmail.com>
2023-11-01 23:27:21 +0000

    working on mobile menu, Offcanvas & accordion used

--- src/components/layouts/FooterSection/Header-mobile.jsx
+++ src/components/layouts/FooterSection/Header-mobile.jsx
@@ -1,20 +1,76 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useState } from "react";
+
+import Accordion from "react-bootstrap/Accordion";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+import userIcon from "../../../assets/images/icons/user.svg";
+import basketIcon from "../../../assets/images/icons/basket.svg";
+import techHeimLogo from "../../../assets/images/icons/logo.svg";
 
 function MobileNavScroll() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Toggle aria-controls="navbarScroll" onClick={handleShow} />
+        <Offcanvas className=" mw-75" show={show} onHide={handleClose}>
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>
+              <img
+                src={techHeimLogo}
+                className="mobile-logo"
+                alt="tech Heim logo"
+              />
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Accordion>
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>Products</Accordion.Header>
+                <Accordion.Body className=" d-flex flex-column">
+                  <ul>
+                    <li>Mobile Phones</li>
+                    <li>Laptops & Computers</li>
+                    <li>Tablets & E-reader</li>
+                    <li>Wearables</li>
+                    <li>Audio</li>
+                    <li>Cameras</li>
+                    <li>Gaming</li>
+                    <li>Networking</li>
+                    <li>Accessories</li>
+                  </ul>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+            <div rel="stylesheet" href="/">
+              Blog
+            </div>
+            <div rel="stylesheet" href="/">
+              FAQ
+            </div>
+            <div rel="stylesheet" href="/">
+              Contact us
+            </div>
+          </Offcanvas.Body>
+        </Offcanvas>
+        <Navbar.Brand href="#" className=" fs-1 m-0">
+          Tech Heim
+        </Navbar.Brand>
+        <div>
+          <img src={userIcon} alt="user icon" />
+          <img src={basketIcon} alt="basket icon" />
+        </div>
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
+            style={{ maxHeight: "100px" }}
             navbarScroll
           >
             <Nav.Link href="#action1">Home</Nav.Link>
@@ -33,17 +89,16 @@ function MobileNavScroll() {
               Link
             </Nav.Link>
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
         </Navbar.Collapse>
       </Container>
+      <Form className="d-flex w-100">
+        <Form.Control
+          type="search"
+          placeholder="What can we help you to find ?"
+          className="me-2"
+          aria-label="Search"
+        />
+      </Form>
     </Navbar>
   );
 }
