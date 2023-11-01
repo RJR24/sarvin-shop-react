749a19c22cf3360249ea8d89296fb3456b66bdef
Kaveh <rezarezaeijamiuk@gmail.com>
2023-11-01 16:06:16 +0000

    on responsiveness

--- src/components/BasketMenu/BasketDropdown.jsx
+++ src/components/BasketMenu/BasketDropdown.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
+import "./basketMenuStyles.css";
 import BasketItemsCard from "./BasketItemsCard";
 
 import checkoutTrolley from "../../assets/images/icons/checkout-trolley.svg";
@@ -8,23 +9,28 @@ import checkoutTrolley from "../../assets/images/icons/checkout-trolley.svg";
 const BasketDropdown = () => {
   const { basket, totalQty } = useSelector((state) => state.cartReducer);
   return (
-    <div className="cartItems width={150px} bg-danger">
+    <>
       <div className="itemsNumber">{totalQty} items</div>
-      {basket.map((item) => (
-        <BasketItemsCard {...item} key={item.id} />
-      ))}
-      <div className="proceed-to-checkout">
-        <div>
+      <div className="cartItems d-flex justify-content-center">
+        <div className="items-container align-items-center">
+          {basket.map((item) => (
+            <BasketItemsCard {...item} key={item.id} />
+          ))}
+        </div>
+      </div>
+      <div className="proceed-to-checkout d-flex gap-4">
+        <div className="total-price">
           Grand total <br />
           $543.02
         </div>
         <div>
-          <button className="btn btn-primary px-4 py-2">
-            Proceed to cart <img src={checkoutTrolley} alt="trolley" />
+          <button className="checkout-btn btn btn-primary d-flex justify-content-center align-items-center gap-2 px-4 py-2">
+            <div> Proceed to cart </div>
+            <img src={checkoutTrolley} alt="trolley" className="" />
           </button>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
