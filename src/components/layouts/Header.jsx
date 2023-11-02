7daa420d3cd51cc39a9b8a1a1af23e187c97dd7c
Kaveh <rezarezaeijamiuk@gmail.com>
2023-11-02 00:50:37 +0000

    header refactored code readability improved

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -4,34 +4,12 @@ import "./header-styles.css";
 
 import logo from "../../assets/images/icons/logo.svg";
 import search from "../../assets/images/icons/search.svg";
-import user from "../../assets/images/icons/user.svg";
-import basket from "../../assets/images/icons/basket.svg";
 
-import profile from "../../assets/images/icons/profile-circle.svg";
-import orders from "../../assets/images/icons/orders.svg";
-import wishList from "../../assets/images/icons/wishList.svg";
-import payments from "../../assets/images/icons/payments.svg";
-import logoutIcon from "../../assets/images/icons/logout.svg";
-
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { login, logout } from "../../redux/slices/userSlice";
-import BasketDropdown from "../BasketMenu/BasketDropdown";
-import MobileNavScroll from "./FooterSection/Header-mobile";
+import MobileNavScroll from "./Header-mobile";
+import UserAccountDropdown from "../User-account/UserAccountDropdown";
+import UserBasketComponent from "../UserBasket/UserBasketComponent";
 
 const Header = () => {
-  const [open, setOpen] = useState(false);
-  // const [basketOpen, setBasketOpen] = useState(false);
-
-  const basketItems = useSelector((state) => state.cartReducer.basket);
-  // console.log(basketItems, "cart item");
-  const username = useSelector((state) => state.userReducer.username);
-  const { totalQty } = useSelector((state) => state.cartReducer);
-  const dispatcher = useDispatch();
-  const userLogin = () => {
-    dispatcher(login({ username: "Kaveh024" }));
-  };
-
   return (
     <>
       <div className="mobile-navbar">
@@ -40,7 +18,7 @@ const Header = () => {
       <header className=" header-styles py-3 ">
         <div className="container d-flex justify-content-between">
           <div>
-            <img src={logo} alt="" width="30" />
+            <img src={logo} alt="tech heim" width="30px" />
           </div>
           <nav className="nav gap-4">
             <Link to="/" className="nav-link text-black blue-hover-color">
@@ -68,7 +46,7 @@ const Header = () => {
               Contact us
             </Link>
           </nav>
-          <div className=" d-flex ">
+          <div className=" d-flex align-items-center ">
             <button className="bg-transparent border-0 pe-3">
               <img src={search} alt="" />
             </button>
@@ -80,68 +58,8 @@ const Header = () => {
             onMouseLeave={() => setBasketOpen(false)}
           > */}
 
-            <div className="basket menu-trigger  d-inline-block">
-              <Link to="" className="pe-3 position-relative">
-                <img src={basket} alt="basket" />
-                <span className="position-absolute bg-danger text-white rounded-circle">
-                  {totalQty}
-                </span>
-              </Link>
-              <div className="basket-dropdown  dropdown-menu ">
-                {basketItems.length ? (
-                  <BasketDropdown />
-                ) : (
-                  <div>
-                    <h3 className="bg-warning" width="100px">
-                      Basket is empty!
-                    </h3>
-                  </div>
-                )}
-              </div>
-            </div>
-
-            <div
-              className={`menu-trigger  d-inline ${
-                open ? "userActive" : "userInactive"
-              }`}
-              onClick={() => {
-                setOpen(!open);
-              }}
-            >
-              <Link to="">
-                <img src={user} alt="user" />
-              </Link>
-
-              <div className="dropdown-menu">
-                {username ? (
-                  <div>
-                    <h3>
-                      {username}
-                      <br />
-                      <span>{username}@gmail.com</span>
-                    </h3>
-                    <ul className="list-unstyled">
-                      <DropdownItem img={profile} text={"My Profile"} />
-                      <DropdownItem img={orders} text={"Orders"} />
-                      <DropdownItem img={wishList} text={"Wish List"} />
-                      <DropdownItem img={payments} text={"Payments"} />
-                      <DropdownItem
-                        img={logoutIcon}
-                        text={"Log Out"}
-                        event={true}
-                      />
-                    </ul>
-                  </div>
-                ) : (
-                  <div>
-                    <button className="btn btn-primary" onClick={userLogin}>
-                      Login
-                    </button>
-                    <button className="btn btn-primary">Register</button>
-                  </div>
-                )}
-              </div>
-            </div>
+            <UserBasketComponent />
+            <UserAccountDropdown />
           </div>
         </div>
       </header>
@@ -149,47 +67,4 @@ const Header = () => {
   );
 };
 
-function DropdownItem(props) {
-  const dispatcher = useDispatch();
-  const userLogout = () => {
-    dispatcher(logout());
-  };
-  return (
-    <>
-      {props.event ? (
-        <li className="dropdownItem" onClick={userLogout}>
-          <img src={props.img} alt="" />
-          <a href="#">{props.text}</a>
-        </li>
-      ) : (
-        <li className="dropdownItem">
-          <img src={props.img} alt="" />
-          <a href="#">{props.text}</a>
-        </li>
-      )}
-    </>
-  );
-}
-
 export default Header;
-
-// <div>
-//                   <h3>
-//                     <span>{username} basket</span>
-//                   </h3>
-//                   <hr />
-//                   {/* <div className="cartItems">
-
-//                   </div> */}
-//                   <ul className="list-unstyled">
-//                     <DropdownItem img={profile} text={"product 1"} />
-//                     <DropdownItem img={orders} text={"product 2"} />
-//                     <DropdownItem img={wishList} text={"product 3"} />
-//                     <DropdownItem img={payments} text={"product 4"} />
-//                     <DropdownItem
-//                       img={logoutIcon}
-//                       text={"Log Out"}
-//                       event={true}
-//                     />
-//                   </ul>
-//                 </div>
