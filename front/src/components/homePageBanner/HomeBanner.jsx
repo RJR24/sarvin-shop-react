f8b8a1989ec96522072dde259cc8af8b272ed66a
Kaveh <rezarezaeijamiuk@gmail.com>
2023-11-18 21:25:05 +0000

    working on responsiveness

--- front/src/components/homePageBanner/HomeBanner.jsx
+++ front/src/components/homePageBanner/HomeBanner.jsx
@@ -9,17 +9,21 @@ import iphone15Mobile from "../../assets/images/banners/iphone15-mobile.svg";
 
 const HomeBanner = () => {
   return (
-    <div className=" row d-flex justify-content-center ">
-      <div className="iphone15 col-md-7 col-12 ">
+    <div className=" row d-flex justify-content-center gap-2">
+      <div className="iphone15 col-md-7  ">
         <picture>
-          <source srcSet={iphone15} media="(min-width : 768px)" />
-          <img src={iphone15Mobile} alt="iphone15" />
+          <source
+            className="iphone-desktop-size"
+            srcSet={iphone15}
+            media="(min-width : 768px)"
+          />
+          <img src={iphone15Mobile} className="mobile-images" alt="iphone15" />
         </picture>
       </div>
-      <div className="ps5  col-md-4 col-12">
+      <div className="ps5 col-md-4 ">
         <picture>
           <source srcSet={ps5} media="(min-width:768px )" />
-          <img src={ps5Mobile} alt="play station" />
+          <img src={ps5Mobile} className="mobile-images" alt="play station" />
         </picture>
       </div>
     </div>
